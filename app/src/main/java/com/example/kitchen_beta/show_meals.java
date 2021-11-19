49cc34cab15100123105c8bf96fee097600bbf63
Yoad <yv8588@bs.k12.amalnet.il>
2021-11-20 00:08:33 +0200

    #1 need to figure out waiter activity layout. #2 need to figure out erase from bon and still saving it. #3 need to finish all options menus. #4 need to work on tv conection.

--- app/src/main/java/com/example/kitchen_beta/show_meals.java
+++ app/src/main/java/com/example/kitchen_beta/show_meals.java
@@ -1,14 +1,10 @@
 package com.example.kitchen_beta;
 
 import static com.example.kitchen_beta.FBref.refActive;
-import static com.example.kitchen_beta.FBref.refBon;
-import static com.example.kitchen_beta.FBref.refMeal;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -16,10 +12,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -28,19 +21,19 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
-public class kitchen_manager extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class show_meals extends AppCompatActivity{
     ArrayList<Bon> meal_read= new ArrayList<Bon>();
     ArrayList<String>meal_view= new ArrayList<String>();
     ListView meal_list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_kitchen_manager);
+        setContentView(R.layout.activity_show_meals);
         meal_list=(ListView)findViewById(R.id.meal_list);
         ArrayAdapter<String> adp=new ArrayAdapter<>(this,R.layout.support_simple_spinner_dropdown_item,meal_view);
         meal_list.setAdapter(adp);
-        meal_list.setOnItemSelectedListener(this);
     }
+
     /**
      * when activity starts gets the meals from db to show on list view.
      */
@@ -71,70 +64,6 @@ public class kitchen_manager extends AppCompatActivity implements AdapterView.On
             meal_view.add(meal_read.get(i).toString());
         }
         meal_list.deferNotifyDataSetChanged();
-
-    }
-
-    /**
-     * lets user erase a bon when done.
-     * @param adapterView
-     * @param view
-     * @param i
-     * @param l
-     */
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        AlertDialog.Builder adb=new AlertDialog.Builder(this);
-        TextView tv=new TextView(this);
-        tv.setText("erase bon?");
-        adb.setNegativeButton("yes", new DialogInterface.OnClickListener() {
-            /**
-             * when clicked erase the bon.
-             * <p>
-             * @param dialog the dialog.
-             */
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Bon b=meal_read.get(i);
-                meal_read.remove(i);
-                refMeal.child("bon").setValue(b);
-                meal_view.remove(i);
-                meal_list.deferNotifyDataSetChanged();
-            }
-        });
-        adb.setNegativeButton("no", new DialogInterface.OnClickListener() {
-            /**
-             * when clicked gets out.
-             * <p>
-             * @param dialog the dialog.
-             */
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Toast.makeText(kitchen_manager.this, "operation canceled", Toast.LENGTH_SHORT).show();
-                dialog.dismiss();
-            }
-        });
-        adb.setNeutralButton("meal",new DialogInterface.OnClickListener(){
-            /**
-             * when clicked erase meal.
-             * <p>
-             * @param dialog the dialog.
-             */
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Intent si=new Intent(kitchen_manager.this,erase.class);
-                si.putExtra("bon",meal_read.get(i));
-                startActivity(si);
-            }
-        });
-        adb.setView(tv);
-        AlertDialog ad=adb.create();
-        ad.show();
-
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
-
     }
     /**
      *creates options menu
@@ -144,6 +73,7 @@ public class kitchen_manager extends AppCompatActivity implements AdapterView.On
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main,menu);
+        menu.add("add bon");
         return super.onCreateOptionsMenu(menu);
     }
     /**
@@ -161,6 +91,10 @@ public class kitchen_manager extends AppCompatActivity implements AdapterView.On
         }
         else if (s.equals("login")){
             si=new Intent(this,MainActivity.class);
+            startActivity(si);
+        }
+        else if(s.equals("add bon")){
+            si=new Intent(this,waiter.class);
             startActivity(si);
         }
         return super.onOptionsItemSelected(item);
