0ce947fe7027c7211c3948bc7c848e4bc599494f
Yoad <yv8588@bs.k12.amalnet.il>
2021-11-24 20:20:22 +0200

    #1 need to finish waiter activity layout(custom adapter ready). #2 need to figure out erase from bon and still saving it. #3 need to finish all options menus. #4 need to work on tv connection.

--- app/src/main/java/com/example/kitchen_beta/CustomAdapter.java
+++ app/src/main/java/com/example/kitchen_beta/CustomAdapter.java
@@ -1,6 +1,11 @@
 package com.example.kitchen_beta;
 
+import static com.example.kitchen_beta.FBref.AUTH;
+import static com.example.kitchen_beta.FBref.storageRef;
+
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +13,14 @@ import android.widget.BaseAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 
@@ -16,26 +29,54 @@ public class CustomAdapter extends BaseAdapter {
     Context context;
     ArrayList<Meal>mealList;
     LayoutInflater inflater;
+
+    /**
+     * constactor.
+     * @param context app context
+     * @param mealList the list.
+     * @param inflater the layout inflater.
+     */
     public CustomAdapter(Context context,ArrayList<Meal>mealList,LayoutInflater inflater){
         this.context=context;
         this.mealList=mealList;
         this.inflater=inflater;
     }
+
+    /**
+     * @return the list size.
+     */
     @Override
     public int getCount() {
         return mealList.size();
     }
 
+    /**
+     *
+     * @param i the item id.
+     * @return the place in the list of the  object.
+     */
     @Override
     public Object getItem(int i) {
         return mealList.get(i);
     }
 
+    /**
+     *
+     * @param i the item in the list.
+     * @return the id
+     */
     @Override
     public long getItemId(int i) {
         return 0;
     }
 
+    /**
+     *
+     * @param i place clicked in list.
+     * @param view the view in the list(row).
+     * @param viewGroup
+     * @return the view.
+     */
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
         view=inflater.inflate(R.layout.custom_lv,null);
@@ -46,8 +87,37 @@ public class CustomAdapter extends BaseAdapter {
         Meal m=mealList.get(i);
         des.setText(m.getAbout());
         name.setText(m.getName());
-        //price.setText();
+        price.setText(((Double) m.getPrice()).toString());
         // set photo from fb here.
+        FirebaseUser user = AUTH.getCurrentUser();
+        if(user!=null) {
+            StorageReference pathReference = storageRef.child("images/" +m.getCategory()+m.getName() + ".jpg");
+
+            final long ONE_MEGABYTE = 1024 * 1024;
+            pathReference.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                /**
+                 * when download is succesfull shows image
+                 * <p>
+                 * @param bytes the byte array (image).
+                 */
+                @Override
+                public void onSuccess(byte[] bytes) {
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(bytes , 0, bytes .length);
+                    photo.setImageBitmap(bitmap);
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                /**
+                 * when download photo from fb fails shows toast to notify user.
+                 * @param exception the failure exception.
+                 */
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    Toast.makeText(context, "cant find image for this meal", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+
         return view;
     }
 }
